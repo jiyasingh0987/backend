d289af90091d754f436b291c81888e58622e6c80
jiyasingh0987 <jiyasingh8383@gmail.com>
2026-09-16 15:26:31 +0530

    feat:updated code

--- lec 16/server.js
+++ lec 16/server.js
@@ -1,36 +1,36 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const app = express();
 const PORT = 3000;
 
 
-// const logmiddleware = (req, res, next) => {
-//     // console.log("hello from middleware");
-//     // req.name = "abhijeet";
-//     console.log("Req url:", req.url, "Time:", new Date().toLocaleString());
-//     // res.send("hello from middleware");
-//     next();
+const logmiddleware = (req, res, next) => {
+    // console.log("hello from middleware");
+    // req.name = "abhijeet";
+    console.log("Req url:", req.url, "Time:", new Date().toLocaleString());
+    // res.send("hello from middleware");
+    next();
 
-// }
+}
 
-const welcomeMiddleware = (req, res, next) => {
-    console.log("welcome to my website");
+const authMiddleware = (req, res, next) => {
+    console.log("Authentication successful  ");
     next();
 }
 
-// app.use(morgan("dev"));
-app.use(morgan("combined"));
-app.use(welcomeMiddleware);
-// app.use(logmiddleware);
+// // app.use(morgan("dev"));
+// app.use(morgan("combined"));
+// app.use(authMiddleware);
+app.use(logmiddleware);
 
 app.get("/", (req, res) => {
     console.log("homepage");
     res.send("hello from get request");
 })
 
-app.get("/about", (req, res) => {
+app.get("/about", authMiddleware, (req, res) => {
     console.log("about page");
     res.send("hello from about page");
 })
 
-app.listen(PORT, () => console.log("server is running"));   
+app.listen(PORT, () => console.log("server is running"));          
