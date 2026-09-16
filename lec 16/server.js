7a722d41ee3932a8b0d0f1832f0f03f6e00e1b05
jiyasingh0987 <jiyasingh8383@gmail.com>
2026-09-16 15:06:51 +0530

    feat:updated topic on middleware

--- lec 16/server.js
+++ lec 16/server.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
-
 const PORT = 3000;
 
 
-const logmiddleware = (req, res, next) => {
-    console.log("hello from middleware");
-    // res.send("hello from middleware");
-    next();
+// const logmiddleware = (req, res, next) => {
+//     // console.log("hello from middleware");
+//     // req.name = "abhijeet";
+//     console.log("Req url:", req.url, "Time:", new Date().toLocaleString());
+//     // res.send("hello from middleware");
+//     next();
 
+// }
+
+const welcomeMiddleware = (req, res, next) => {
+    console.log("welcome to my website");
+    next();
 }
 
-app.use(logmiddleware);
+// app.use(morgan("dev"));
+app.use(morgan("combined"));
+app.use(welcomeMiddleware);
+// app.use(logmiddleware);
 
 app.get("/", (req, res) => {
     console.log("homepage");
